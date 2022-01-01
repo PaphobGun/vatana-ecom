5762940f9c6f7ce98bdb7d012afdf021107a5863
Paphob <paphob.a@gmail.com>
2022-01-01 08:51:41 +0700

    add layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="default-layout">
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content><Nuxt /></a-layout-content>
+      <a-layout-footer>Footer</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.default-layout {
+}
+</style>
